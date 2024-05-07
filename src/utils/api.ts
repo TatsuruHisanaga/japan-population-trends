d48791a00a0964238062e72a52499b37549f9b38
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-07 23:48:31 +0900

    feat: set appropriate RESAS_API_KEY

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const API_KEY = 'xx'
+const API_KEY = import.meta.env.VITE_RESAS_API_KEY
 const API_URL = 'https://opendata.resas-portal.go.jp'
 
 export const fetchPrefectures = async () => {
   const response = await axios.get(`${API_URL}/api/v1/prefectures`, {
-    headers: { 'X-API-KEY': API_KEY },
+    headers: { 'X-API-KEY': API_KEY }
   })
   return response.data.result
 }
