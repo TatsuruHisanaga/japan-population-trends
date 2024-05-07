a61b6214f98c619b0a50d9bfde0a4139ffbad54b
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-07 23:44:02 +0900

    feat: create HomePage to show prefecture data

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>都道府県一覧</h1>
+    <ul>
+      <li v-for="prefecture in prefecturesStore.prefectures" :key="prefecture.prefCode">
+        {{ prefecture.prefName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { usePrefecturesStore } from '@/stores/prefectures'
+
+export default defineComponent({
+  setup() {
+    const prefecturesStore = usePrefecturesStore()
+
+    prefecturesStore.fetchPrefectures()
+
+    return {
+      prefecturesStore
+    }
+  }
+})
+</script>
