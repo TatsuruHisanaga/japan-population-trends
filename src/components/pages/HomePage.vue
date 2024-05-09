779a426c50c02feabbda68641224b4fdf9948d88
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-09 18:51:21 +0900

    feat: add PrefectureCheckboxList component

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
     <h1>都道府県一覧</h1>
-    <ul>
-      <li v-for="prefecture in prefecturesStore.prefectures" :key="prefecture.prefCode">
-        {{ prefecture.prefName }}
-      </li>
-    </ul>
+    <PrefectureCheckboxList
+      :prefectures="prefecturesStore.prefectures"
+      @change="onPrefectureChange"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { usePrefecturesStore } from '@/stores/prefectures'
+import PrefectureCheckboxList from '@/components/molecules/PrefectureCheckboxList.vue'
 
 export default defineComponent({
+  components: {
+    PrefectureCheckboxList
+  },
   setup() {
     const prefecturesStore = usePrefecturesStore()
 
     prefecturesStore.fetchPrefectures()
 
+    const onPrefectureChange = async (selectedPrefectures: number[]) => {
+      console.log('Selected Prefectures:', selectedPrefectures)
+      // TODO: 選択された都道府県の人口構成を取得する処理を追加
+    }
     return {
-      prefecturesStore
+      prefecturesStore,
+      onPrefectureChange
     }
   }
 })
