e574a73a16ace696f8e83495af876b4e8d108126
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-09 18:52:02 +0900

    feat: create CheckboxItem

---
+++ src/components/atoms/CheckboxItem.vue
@@ -0,0 +1,29 @@
+<!-- components/atoms/CheckboxItem.vue -->
+<template>
+  <label>
+    <input
+      type="checkbox"
+      :value="value"
+      :checked="checked"
+      @change="$emit('change', $event.target.checked)"
+    />
+    <slot></slot>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: [String, Number],
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
