64055aadfce321fd6a4680b260f68d037d676fe5
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-13 00:43:12 +0900

    feat: implement population composition graph using Recharts.js
    Import necessary components from Recharts.js (LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend).
    
    Register Recharts.js components in the components option.
    
    Define populationCompositionData in the computed option to transform the populationComposition prop into the data format required by Recharts.js.

--- src/components/molecules/PopulationCompositionGraph.vue
+++ src/components/molecules/PopulationCompositionGraph.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <!-- グラフコンポーネントの実装 -->
+    <LineChart :width="600" :height="400" :data="populationCompositionData" >
+      <XAxis dataKey="year" />
+      <YAxis />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+    </LineChart>
   </div>
 </template>
 
@@ -8,15 +15,34 @@
 import { defineComponent } from 'vue';
 import type { PropType } from 'vue';
 import type { PopulationComposition } from '@/types/populationComposition';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 export default defineComponent({
   name: 'PopulationCompositionGraph',
+  components: {
+    LineChart,
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Legend,
+  },
   props: {
     populationComposition: {
       type: Array as PropType<PopulationComposition[]>,
       required: true,
     },
   },
-  // グラフコンポーネントの実装
+  computed: {
+    populationCompositionData(): { year: number; value: number }[] {
+      return this.populationComposition.map((data) => ({
+        year: data.year,
+        value: data.value,
+      }));
+    },
+  },
 });
 </script>
