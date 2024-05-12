b5f8ee256ee3d4670ab829357545823fab62385b
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-05-13 00:21:53 +0900

    feat: create component to show population data

---
+++ src/components/molecules/PopulationCompositionGraph.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- グラフコンポーネントの実装 -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+import type { PopulationComposition } from '@/types/populationComposition';
+
+export default defineComponent({
+  name: 'PopulationCompositionGraph',
+  props: {
+    populationComposition: {
+      type: Array as PropType<PopulationComposition[]>,
+      required: true,
+    },
+  },
+  // グラフコンポーネントの実装
+});
+</script>
